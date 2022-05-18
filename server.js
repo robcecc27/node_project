81cf968c54aaaef4af8d4754edb46e998eabd8fe
robcecc27 <rob.cecchini@gmail.com>
2022-05-17 19:37:54 -0500

    changed webpage message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('A simple Dockerized Node.js app launched via Terraform');
 });
 
 app.listen(PORT, HOST);
